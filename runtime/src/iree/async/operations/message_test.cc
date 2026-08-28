25103313ad27130c03c6f90a90d7d2aa46fa3b72
Ben Vanik <ben.vanik@gmail.com>
2026-08-28 15:49:11 -0700

    [Runtime] Initialize message test operations in place

--- runtime/src/iree/async/operations/message_test.cc
+++ runtime/src/iree/async/operations/message_test.cc
@@ -18,43 +18,46 @@ static void ConsumeCompletion(void* user_data,
   iree_status_free(status);
 }
 
-static iree_async_message_operation_t MakeMessage() {
-  iree_async_message_operation_t message = {};
+static void InitializeMessage(iree_async_message_operation_t* message) {
   iree_async_operation_initialize(
-      &message.base, IREE_ASYNC_OPERATION_TYPE_MESSAGE,
+      &message->base, IREE_ASYNC_OPERATION_TYPE_MESSAGE,
       IREE_ASYNC_OPERATION_FLAG_NONE, ConsumeCompletion, nullptr);
-  message.target = reinterpret_cast<iree_async_proactor_t*>(uintptr_t{1});
-  return message;
+  message->target = reinterpret_cast<iree_async_proactor_t*>(uintptr_t{1});
 }
 
 TEST(MessageOperationTest, AcceptsSourceCompletion) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   IREE_EXPECT_OK(iree_async_message_operation_validate(&message));
 }
 
 TEST(MessageOperationTest, RequiresTarget) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.target = nullptr;
   IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
                         iree_async_message_operation_validate(&message));
 }
 
 TEST(MessageOperationTest, SourceCompletionRequiresCallback) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.base.completion_fn = nullptr;
   IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
                         iree_async_message_operation_validate(&message));
 }
 
 TEST(MessageOperationTest, AcceptsSuppressedSourceCompletion) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.message_flags = IREE_ASYNC_MESSAGE_FLAG_SKIP_SOURCE_COMPLETION;
   message.base.completion_fn = nullptr;
   IREE_EXPECT_OK(iree_async_message_operation_validate(&message));
 }
 
 TEST(MessageOperationTest, SuppressedCompletionMustBeChainTail) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.message_flags = IREE_ASYNC_MESSAGE_FLAG_SKIP_SOURCE_COMPLETION;
   message.base.flags = IREE_ASYNC_OPERATION_FLAG_LINKED;
   message.base.completion_fn = nullptr;
@@ -63,14 +66,16 @@ TEST(MessageOperationTest, SuppressedCompletionMustBeChainTail) {
 }
 
 TEST(MessageOperationTest, SuppressedCompletionRejectsCallback) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.message_flags = IREE_ASYNC_MESSAGE_FLAG_SKIP_SOURCE_COMPLETION;
   IREE_EXPECT_STATUS_IS(IREE_STATUS_INVALID_ARGUMENT,
                         iree_async_message_operation_validate(&message));
 }
 
 TEST(MessageOperationTest, SuppressedCompletionRejectsPool) {
-  iree_async_message_operation_t message = MakeMessage();
+  iree_async_message_operation_t message = {};
+  InitializeMessage(&message);
   message.message_flags = IREE_ASYNC_MESSAGE_FLAG_SKIP_SOURCE_COMPLETION;
   message.base.completion_fn = nullptr;
   message.base.pool =
